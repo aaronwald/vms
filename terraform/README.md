a4c374c6a62598177e1d159c8ab796cf1960d1e0
Aaron Wald <aaronwald@gmail.com>
2019-01-15 12:15:50 +0000

    Update rollout to deployments

--- terraform/README.md
+++ terraform/README.md
@@ -3,3 +3,4 @@ Setup credentials
 ```
 gcloud container clusters get-credentials coypu-cluster --zone us-east1-b
 ```
+
--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,26 @@ provider "google" {
   zone    = "us-east1-b"
 }
 
+provider "kubernetes" {
+  host                   = "${google_container_cluster.primary.endpoint}"
+  username               = "${var.master_username}"
+  password               = "${var.master_password}"
+  client_certificate     = "${base64decode(google_container_cluster.primary.master_auth.0.client_certificate)}"
+  client_key             = "${base64decode(google_container_cluster.primary.master_auth.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
+}
+
+provider "helm" {
+  kubernetes {
+	 host                   = "${google_container_cluster.primary.endpoint}"
+	 username               = "${var.master_username}"
+	 password               = "${var.master_password}"
+	 client_certificate     = "${base64decode(google_container_cluster.primary.master_auth.0.client_certificate)}"
+	 client_key             = "${base64decode(google_container_cluster.primary.master_auth.0.client_key)}"
+	 cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
+  }
+}
+
 resource "google_container_cluster" "primary" {
   name               = "coypu-cluster"
   zone               = "us-east1-b"
@@ -36,28 +56,6 @@ resource "google_container_cluster" "primary" {
   network = "${google_compute_network.vpc_network.name}"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-
-  boot_disk {
-	 initialize_params {
-		image = "debian-cloud/debian-9"
-	 }
-  }
-
-  network_interface {
-	 network       = "${google_compute_network.vpc_network.self_link}"
-	 access_config = {
-		// give external ip
-	 }
-  }
-
-  metadata {
-	 sshKeys = "wald:${file("~/.ssh/id_rsa.pub")}"
-  }
-}
-
 resource "google_compute_firewall" "default" {
   name    = "test-firewall"
   network = "${google_compute_network.vpc_network.name}"
@@ -77,48 +75,6 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = "true"
 }
 
-
-provider "kubernetes" {
-  host                   = "${google_container_cluster.primary.endpoint}"
-  username               = "${var.master_username}"
-  password               = "${var.master_password}"
-  client_certificate     = "${base64decode(google_container_cluster.primary.master_auth.0.client_certificate)}"
-  client_key             = "${base64decode(google_container_cluster.primary.master_auth.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
-}
-
-provider "helm" {
-  kubernetes {
-	 host                   = "${google_container_cluster.primary.endpoint}"
-	 username               = "${var.master_username}"
-	 password               = "${var.master_password}"
-	 client_certificate     = "${base64decode(google_container_cluster.primary.master_auth.0.client_certificate)}"
-	 client_key             = "${base64decode(google_container_cluster.primary.master_auth.0.client_key)}"
-	 cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
-  }
-}
-
-
-resource "kubernetes_pod" "nginx" {
-  metadata {
-	 name = "nginx-example"
-	 labels {
-		App = "nginx"
-	 }
-  }
-
-  spec {
-	 container {
-		image = "nginx:1.15.2"
-		name  = "example"
-
-		port {
-		  container_port = 80
-		}
-	 }
-  }
-}
-
 resource "kubernetes_secret" "docker-registry" {
   metadata {
     name = "docker-registry"
@@ -131,6 +87,49 @@ resource "kubernetes_secret" "docker-registry" {
   type = "kubernetes.io/dockerconfigjson"
 }
 
+resource "kubernetes_deployment" "coypu_server" {
+  metadata {
+	 name = "coypu-server"
+	 labels {
+		App = "coypu"
+	 }
+  }
+
+  spec {
+	 replicas = 3
+
+	 selector {
+		match_labels {
+		  App = "coypu"
+		}
+	 }
+
+	 template {
+		metadata {
+		  labels {
+			 App = "coypu"
+		  }
+		}
+
+		spec {
+		  container {
+			 name  = "coypu"
+			 image = "gcr.io/massive-acrobat-227416/coypu:latest"
+			 
+			 port {
+				container_port = 8080
+			 }
+		  }
+
+		  image_pull_secrets =  {
+			 name =  "docker-registry"
+		  }
+		}
+	 }
+  }
+}
+
+/*
 resource "kubernetes_pod" "coypu" {
   metadata {
 	 name = "coypu-example"
@@ -155,23 +154,7 @@ resource "kubernetes_pod" "coypu" {
 	 }
   }
 }
-
-resource "kubernetes_service" "nginx" {
-  metadata {
-	 name = "nginx-example"
-  }
-  spec {
-	 selector {
-		App = "${kubernetes_pod.nginx.metadata.0.labels.App}"
-	 }
-	 port {
-		port = 80
-		target_port = 80
-	 }
-
-	 type = "LoadBalancer"
-  }
-}
+*/
 
 resource "kubernetes_service" "coypu" {
   metadata {
@@ -179,7 +162,7 @@ resource "kubernetes_service" "coypu" {
   }
   spec {
 	 selector {
-		App = "${kubernetes_pod.coypu.metadata.0.labels.App}"
+		App = "coypu"
 	 }
 	 port {
 		port = 80
@@ -190,28 +173,7 @@ resource "kubernetes_service" "coypu" {
   }
 }
 
-
-/* need service account
-resource "helm_release" "mydatabase" {
-    name      = "nutriadb"
-    chart     = "stable/postgresql"
-
-    set {
-        name  = "postgresqlUsername"
-        value = "foo"
-    }
-
-    set {
-        name = "postgresqlPassword"
-        value = "qux"
-    }
-}
-*/
-
-output "ip" {
-  value = "${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}"
-}
-
+/*
 output "client_certificate" {
   value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
 }
@@ -223,11 +185,8 @@ output "client_key" {
 output "cluster_ca_certificate" {
   value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
 }
-
-output "lb_ip" {
-  value = "${kubernetes_service.nginx.load_balancer_ingress.0.ip}"
-}
+*/
 
 output "coypu_ip" {
-  value = "${kubernetes_service.coypu.spec.0.cluster_ip}"
+  value = "${kubernetes_service.coypu.load_balancer_ingress.0.ip}"
 }
